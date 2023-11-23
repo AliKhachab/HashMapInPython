f80af5aa08ae68e65997b4015872ec5fee9d6123
AliKhachab <a.khachab620@gmail.com>
2023-11-22 23:04:44 -0500

    Fixed code slowdowns, added dosctrings, added comments, removed redundant or useless code

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
-#from enum import Enum
 import math
 
-
 class HashMap:
-
-    # Note to self for enum:
-    # Enum works similar to Java (not like I've used it there but whatever)
-    # Where it lets you input letters, and it treats it like some integer
-    # To get the actual letters do ClassName.(enumName). To get the value, add .value at the end
-
     def __init__(self, size: int = 100) -> None:
+        '''
+        @param: size (int). Default value of 100.
+        Constructor
+
+        Makes the hash map.
+        '''
+
         self.__size = size
         self.__map = [[] for _ in range(self.__size)]
-        self.__load = 0
-        self.__weight = 0
+        self.__load = 0 # load as in weight/size. see resize method.
+        self.__weight = 0 # weight as in how many key value pairs are in the hash map.
 
     def __str__(self) -> str:
+        '''toString'''
+
         res = "{"
         for item in self.__map:
             if item != [] and item != [None, None]:
@@ -24,28 +25,42 @@ class HashMap:
         res += "}" if res != "" else ""
         return res
 
-    def insertOrReplace(self, key, value) -> bool:
+    def insertOrReplace(self, key, value):
+        '''
+        @param: key (any type)
+        @param: value (any type)
+        Note: key and value cannot both be None
+
+        Inserts the key and value into the hash map.
+        If the key already exists, it replaces the value for that key with the new value.
+        '''
+
         if key is None and value is None:
             print("No NoneType attributes can be added to this HashMap as a key and value pair.")
-            return False
+            return
+        
         # check if key in map at hashed index is possible for insertion
         index = hash(key) % self.__size
+
         # if empty bucket at index
         if self.__map[index] == [] or self.__map[index] == [None, None]:
             self.__map[index] = [key, value]
             self.__weight += 1
+
+            # increases weight, sets new load, and resizes if need be.
+
             self.__setLoad()
             self.__resize()
-            return True
+            return
 
-        # else if there is a full bucket but the keys match, replace
+        # else if there is a full bucket but the keys match, replace.
+        # no need to change weight because we are just replacing a key value pair. 
+
         elif len(self.__map[index]) == 2:
             if self.__map[index][0] == key:
                 self.__map[index] = [key, value]
-                self.__weight += 1
-                self.__setLoad()
-                self.__resize()
-                return True
+                return
+        
         # else linear probe and find a bucket
         counter = (index + 1) % self.__size
         b = False
@@ -56,23 +71,38 @@ class HashMap:
                     self.__weight += 1
                     self.__setLoad()
                     self.__resize()
-                    return True
+                    return
             if counter == index:
                 b = True
+                break
             else:
                 counter = (counter + 1) % self.__size
 
-        if b:
-            return False  # this accounts for a bug if the thing is full. if my code is right it won't
-        # ever happen but you know. stuff happens.
-        else:
+        if not b:
+            # linear probing worked, we found an empty bucket or a bucket with a matching key -- update that index
+            # to store [key, value]
             self.__map[counter] = [key, value]
             self.__weight += 1
             self.__setLoad()
             self.__resize()
-            return True
+            return
+        
+        else:
+        # this accounts for a bug where the hash map load resizing protocol is 
+            # bugged and for some reason the hash map is full. this *shouldn't be happening* though.
+            self.__load = 1.0 #force override the load value to trigger self.resize()'s if statement
+            self.__resize() # do self.resize()
+            self.insertOrReplace(key, value) #recursive call now that there is space in the hash map
+            return
 
-    def find(self, key):
+            
+
+    def find(self, key) -> bool:
+        '''
+        @param: key (any type)
+
+        Checks if key is in map.
+        '''
         index = hash(key) % self.__size
         if self.__map[index][0] == key:
             return True
@@ -82,6 +112,11 @@ class HashMap:
             return True
 
     def get(self, key):
+        '''
+        @param: key (any type)
+
+        Gets the value for the following key.
+        '''
         index = hash(key) % self.__size
         if self.__map[index][0] == key:
             return self.__map[index][1]
@@ -93,6 +128,11 @@ class HashMap:
                 print(f"~~~No key value pair with the key {key} was found.~~~")
 
     def delete(self, key) -> None:
+        '''
+        @param: key (any type)
+
+        Deletes a key value pair from the map.
+        '''
         index = hash(key) % self.__size
         if self.__map[index][0] == key:
             self.__map[index] = [None, None]
@@ -108,6 +148,11 @@ class HashMap:
                 print(f"~~~No key value pair with the key {key} was found.~~~")
 
     def _linearProbe(self, key) -> int:
+        '''
+        @param: key
+        
+        Method used for linear probing.
+        '''
         index = hash(key) % self.__size
         counter = (index + 1) % self.__size
         while self.__map[counter] != [] and counter != index:
@@ -116,10 +161,10 @@ class HashMap:
             else:
                 counter = (counter + 1) % self.__size
         return -1
-
-    def __resize(self) -> None:
-        if self.__load > 0.75:
-            def isPrime(n):
+    
+    # used in finding next array size for the hash map. not writing docstrings for these because this code isn't mine.
+    @staticmethod
+    def __isPrime(n):
 
                 # Corner cases
                 if (n <= 1):
@@ -137,30 +182,36 @@ class HashMap:
                         return False
 
                 return True
+    @staticmethod
+    def __nextPrime(N):
 
-            def nextPrime(N):
+        # Base case
+        if (N <= 1):
+            return 2
 
-                # Base case
-                if (N <= 1):
-                    return 2
+        prime = N
+        found = False
 
-                prime = N
-                found = False
+        # Loop continuously until isPrime returns
+        # True for a number greater than n
+        while (not found):
+            prime = prime + 1
 
-                # Loop continuously until isPrime returns
-                # True for a number greater than n
-                while (not found):
-                    prime = prime + 1
+            if (HashMap.__isPrime(prime) == True):
+                found = True
 
-                    if (isPrime(prime) == True):
-                        found = True
+        return prime
+    
+    
+            # above methods for primes taken from https://www.geeksforgeeks.org/program-to-find-the-next-prime-number/#
 
-                return prime
-
-            # above inner functions taken from https://www.geeksforgeeks.org/program-to-find-the-next-prime-number/#
-            # because I'm focusing on hash map stuff not finding new primes
-
-            num = nextPrime(self.__size)
+    
+    def __resize(self) -> None:
+        '''
+        Resizes hash map when load is too big.
+        '''
+        if self.__load > 0.75:
+            num = HashMap.__nextPrime(self.__size)
             keys = self.getKeys()
             values = self.getValues()
             self.__map = [[] for _ in range(num)]
@@ -173,9 +224,15 @@ class HashMap:
             print(f"Resize has occurred. New size is {self.__size}")
 
     def __setLoad(self) -> None:
+        '''
+        Sets load. Meant to be used when item is inserted or deleted.
+        '''
         self.__load = self.__weight / self.__size
 
     def getKeys(self) -> []:
+        '''
+        Returns all keys.
+        '''
         res = []
         for item in self.__map:
             if item != [] and item != [None, None]:
@@ -183,6 +240,7 @@ class HashMap:
         return res
 
     def getValues(self):
+        '''Returns all values.'''
         res = []
         for item in self.__map:
             if item != [] and item != [None, None]:
@@ -190,6 +248,7 @@ class HashMap:
         return res
 
     def getLoad(self):
+        
         return self.__load
 
 hm = HashMap(5)
@@ -197,4 +256,4 @@ for i in range(0, 10, 2):
     hm.insertOrReplace(i, i+1)
 
 print(hm)
-print(hm.getLoad())
+print(hm.getLoad()) # getLoad was just for me to check if my resizing worked. basically debugging tool.
